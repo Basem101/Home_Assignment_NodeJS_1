1f6fd4f661286b9d648e7841800ac5509f042c71
basem.morkos@redcross.ca <bmorkos@redcross.ca>
2018-08-09 13:54:44 -0400

    complete home assignment #1

---
+++ index.js
@@ -0,0 +1,68 @@
+// primary file for "Hello World!" API"
+
+// dependencies
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+
+// instantiate httpServer
+const httpServer = http.createServer(function(req, res) {
+
+	// get the url object
+	const parsedURL = url.parse(req.url, true);
+	// get pathname
+	const pathName = parsedURL.pathname;
+	// get trimmed path
+	const trimmedPath = pathName.replace(/^\/+|\/+$/g, '');
+	// get request method
+	const method = req.method.toLowerCase();
+
+	const decoder = new StringDecoder('utf-8');
+	let buffer = '';
+	req.on('data', function (data) {
+		buffer += decoder.write(data);
+	});
+
+	req.on('end', function () {
+		buffer += decoder.end();
+		// select a handler for each route. if route is not defined, fire notFound handler
+		var selectedHandler = typeof(router[trimmedPath]) !== 'undefined' && method === 'post' ? router[trimmedPath] : handlers.notFound;
+		// execute selectHandler
+		selectedHandler(function(statusCode, data) {
+			// validate statusCode type
+			statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+			// validate data type
+			data = typeof(data) === 'object' ? data : {};
+			console.log('data: ', data);
+			// stringify data
+			var dataStr = JSON.stringify(data)
+			res.setHeader('Content-type', 'application/json');
+			res.writeHead(statusCode);
+			res.end(dataStr);
+		});
+	});
+
+});
+
+// define handlers container
+var handlers = {};
+
+// hello handler
+handlers.hello = function(callback) {
+	callback(200, {"message": "welcome! this is my first home assignment :)"});
+};
+
+// notFound handler
+handlers.notFound = function(callback) {
+	callback(404);
+};
+
+// create router object. define hellp route
+var router = {
+	'hello': handlers.hello
+};
+
+// execute http server
+httpServer.listen(3000, function(){
+	console.log('server is running on port 3000');
+});
